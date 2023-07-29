66fbbdd85063c7b3f7683e228aff34d569f41e58
Rustikum <133569938+Rustikum@users.noreply.github.com>
2023-07-29 14:26:44 +0300

    commit chat

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -1,81 +1,49 @@
-class Autocomplete {
-  constructor( container ) {
-    this.container = container;
-    this.input = container.querySelector( '.autocomplete__input' );
-    this.searchInput = container.querySelector( '.autocomplete__search' );
-    this.list = container.querySelector( '.autocomplete__list' );
-    this.valueContainer = container.querySelector( '.autocomplete__value' );
-    this.valueElement = container.querySelector( '.autocomplete__text-content' );
+'use strict';
 
-    this.registerEvents();
+const messages = [
+  'Кто тут?',
+  'Где ваша совесть?',
+  'Добрый день! До свидания!',
+  'Мы ничего не будем вам продавать',
+  'К сожалению, все операторы сейчас заняты. Не пишите нам больше',
+  'Вы не купили ни одного товара для того, чтобы так с нами разговаривать',
+  'Вы кто такие? Я вас не звал! Идит'
+];
+
+const redBadge = document.querySelector('.chat-widget__side');
+const chatWindow = document.querySelector('.chat-widget');
+const chatMessages = document.getElementById('chat-widget__messages');
+const inputMessage = document.getElementById('chat-widget__input');
+
+const initTimer = 30;
+
+const timer = {
+  counter: initTimer,
+  repeat: () => {
+    timer.counter--;
+    if (timer.counter <= 0)
+      robotReply();
   }
+};
 
-  registerEvents() {
-    this.valueContainer.addEventListener( 'click', e => {
-      this.searchInput.classList.add( 'autocomplete__search_active' );
-      this.list.classList.add( 'autocomplete__list_active' );
-      this.searchInput.value = this.valueElement.textContent.trim();
-      this.searchInput.focus();
-
-      this.onSearch();
-    });
-
-
-    this.searchInput.addEventListener( 'input', e => this.onSearch());
-
-    this.list.addEventListener( 'click', e => {
-      const { target } = e;
-      if ( !target.matches( '.autocomplete__item' )) {
-        return;
-      }
-
-      const { textContent: text } = target,
-        { id: value, index } = target.dataset;
-
-      this.onSelect({
-        index,
-        text,
-        value
-      });
-    });
-  }
-
-  onSelect( item ) {
-    this.input.selectedIndex = item.index;
-    this.valueElement.textContent = item.text;
-
-    this.searchInput.classList.remove( 'autocomplete__search_active' );
-    this.list.classList.remove( 'autocomplete__list_active' );
-  }
-
-  onSearch() {
-    const matches = this.getMatches( this.searchInput.value );
-
-    this.renderMatches( matches );
-  }
-
-  renderMatches( matches ) {
-    const html = matches.map( item => `
-    	<li>
-        <span class="autocomplete__item"
-        	data-index="${item.index}"
-          data-id="${item.value}"
-        >${item.text}</span>
-      </li>
-    `);
-
-    this.list.innerHTML = html.join('');
-  }
-
-  getMatches(text) {
-    let result = [];
-    
-    for (const option of this.input.options)
-      if (option.textContent.toLowerCase().includes(text.toLowerCase()))
-        result.push({ text: option.textContent, value: option.value});
-
-    return result;
-  }
+const robotReply = () => {
+  timer.counter = initTimer;
+  const index = Math.floor(Math.random() * messages.length);
+  chatMessages.innerHTML += `<div class="message"><div class="message__time">${new Date().toLocaleTimeString().substr(0,5)}</div><div class="message__text">${messages[index]}</div></div>`;
+  chatMessages.lastElementChild.scrollIntoView(false);
 }
 
-new Autocomplete( document.querySelector( '.autocomplete' ));
+redBadge.addEventListener('click', () => {
+  chatWindow.classList.add('chat-widget_active');  
+  setInterval(timer.repeat, 1000);
+});
+
+inputMessage.addEventListener('keydown', function(e) {
+  if (e.code !== 'Enter')
+    return;
+  if (this.value.length === 0)
+    return;
+  chatMessages.innerHTML += `<div class="message message_client"><div class="message__time">${new Date().toLocaleTimeString().substr(0,5)}</div><div class="message__text">${this.value}</div></div>`;  
+  this.value = '';
+  robotReply();
+});
