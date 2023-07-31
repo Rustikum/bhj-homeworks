1bb9c954cf863eefb7ac36cbc73e0c84356253f8
Rustikum <133569938+Rustikum@users.noreply.github.com>
2023-07-31 13:05:26 +0300

    commit progressbar

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const progress = document.getElementById('progress');
+
+form.addEventListener('submit', check);
+
+function check(event) {
+
+    event.preventDefault();
+
+    const form = document.getElementById('form');
+    const formData = new FormData(form);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+    xhr.upload.onprogress = function (event) {
+
+        progress.value = event.loaded / event.total;
+    }
+
+    xhr.upload.onload = function() {
+        alert("Данные полностью загружены на сервер!");
+    }
+
+    xhr.send(formData);
+}
